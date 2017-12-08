2963522b556e142e30390e337cfb48fe8e5f7291
Michal Mach <michal@mach.org.pl>
2017-12-08 13:22:39 +0100

    from now on we're looping over definition sets for color and size

--- prototype1/generate-image.php
+++ prototype1/generate-image.php
@@ -6,82 +6,143 @@ $count = $obj[0]->count;
 $goal = 1000;
 $progress = $count / $goal;
 
-$imageWidth = 480;
-$imageHeight = 75;
-$colorTransparent = "white";//new ImagickPixel("none");
 
-$goalBarHeight = 25;
-$goalBarWidth = 420;
-$goalBarRounding = 5;
+// Color versions
 
-$goalBarLeftMargin = $goalBarRightMargin = ($imageWidth - $goalBarWidth) / 2;
-$goalBarTopMargin = ($imageHeight - $goalBarHeight) / 2 ;
-$goalBarBottomMargin = ($imageHeight - $goalBarHeight) / 2;
+$tealSet = array(
+  "name" => "teal",
+  "done" => "#07B1B2",
+  "upcoming" => "#82D8D9",
+  "divider" => "#312783");
 
-$triangleSize = 4;
-$triangleOffset = 2;
+$purpleSet = array(
+  "name" => "purple",
+  "done" => "#941B80",
+  "upcoming" => "#D45BC0",
+  "divider" => "#640050");
 
+$yellowSet = array(
+  "name" => "yellow",
+  "done" => "#E67900",
+  "upcoming" => "#F0E500",
+  "divider" => "#C61215");
 
-$bottomTextOffset = 17;
-$topTextOffset = 8;
+$colorSets = array($tealSet, $purpleSet, $yellowSet);
+$colorTransparent = new ImagickPixel("none");
 
-// Don't get longer than max
-$progressChange = $progress < 1 ?
-  $goalBarLeftMargin + ($progress * $goalBarWidth) : $goalBarLeftMargin + $goalBarWidth;
+// Size versions
 
-$draw = new ImagickDraw();
-$draw->setFontSize(15);
-$draw->setStrokeAntialias(true);
-$draw->setTextAntialias(true);
-$draw->setTextAlignment(\Imagick::ALIGN_LEFT);
-$draw->annotation($goalBarLeftMargin, $imageHeight - $goalBarBottomMargin + $bottomTextOffset, "0");
-$draw->setTextAlignment(\Imagick::ALIGN_RIGHT);
-$draw->annotation($imageWidth - $goalBarRightMargin, $imageHeight - $goalBarBottomMargin + $bottomTextOffset, "Goal: $goal");
-$draw->setTextAlignment(\Imagick::ALIGN_CENTER);
-$draw->annotation($progressChange, $goalBarTopMargin - $topTextOffset, "$count");
+$regularSize = array(
+  "name" => "regular",
+  "imageWidth" => 480,
+  "imageHeight" => 75,
+  "goalBarHeight" => 25,
+  "goalBarWidth" => 420,
+  "goalBarRounding" => 5,
+  "triangleSize" => 4,
+  "triangleOffset" => 2,
+  "bottomTextOffset" => 17,
+  "topTextOffset" => 8
+);
 
-
-$draw->setStrokeColor("green");
-$draw->setFillColor("green");
-$draw->roundRectangle (
-  $goalBarLeftMargin , $goalBarTopMargin ,
-  $progressChange , $imageHeight - $goalBarBottomMargin ,
-  $goalBarRounding , $goalBarRounding );
-
-$draw->setStrokeColor("lightgreen");
-$draw->setFillColor("lightgreen");
-$draw->roundRectangle (
-  $progressChange , $goalBarTopMargin ,
-  $imageWidth - $goalBarRightMargin , $imageHeight - $goalBarBottomMargin ,
-  $goalBarRounding , $goalBarRounding );
-
-$topTriangle = array(
-  [ 'x' => $progressChange - $triangleSize, 'y' => $goalBarTopMargin - $triangleOffset],
-  [ 'x' => $progressChange + $triangleSize, 'y' => $goalBarTopMargin - $triangleOffset],
-  [ 'x' => $progressChange , 'y' => $goalBarTopMargin + $triangleSize - $triangleOffset] );
-$bottomTriangle = array(
-  [ 'x' => $progressChange - $triangleSize, 'y' => $imageHeight - $goalBarBottomMargin + $triangleOffset ],
-  [ 'x' => $progressChange + $triangleSize, 'y' => $imageHeight - $goalBarBottomMargin + $triangleOffset],
-  [ 'x' => $progressChange , 'y' => $imageHeight - $goalBarBottomMargin - $triangleSize + $triangleOffset] );
+$sizeSets = array($regularSize);
 
 
-$draw->setStrokeAntialias(false);
-$draw->setStrokeColor("darkgreen");
-$draw->setFillColor("darkgreen");
-$draw->polygon( $topTriangle );
-$draw->polygon( $bottomTriangle );
-$draw->line($topTriangle[2]['x'], $topTriangle[2]['y'], $bottomTriangle[2]['x'], $bottomTriangle[2]['y']);
+foreach ($sizeSets as $sizeSet) {
+
+  $imageWidth = $sizeSet["imageWidth"];
+  $imageHeight = $sizeSet["imageHeight"];
+  $goalBarHeight = $sizeSet["goalBarHeight"];
+  $goalBarWidth = $sizeSet["goalBarWidth"];
+  $goalBarRounding = $sizeSet["goalBarRounding"];
+  $triangleSize = $sizeSet["triangleSize"];
+  $triangleOffset = $sizeSet["triangleOffset"];
+  $bottomTextOffset = $sizeSet["bottomTextOffset"];
+  $topTextOffset = $sizeSet["topTextOffset"];
+
+  $goalBarLeftMargin = $goalBarRightMargin = ($imageWidth - $goalBarWidth) / 2;
+  $goalBarTopMargin = ($imageHeight - $goalBarHeight) / 2 ;
+  $goalBarBottomMargin = ($imageHeight - $goalBarHeight) / 2;
+
+  // Don't get longer than max
+  $progressChange = $progress < 1 ?
+    $goalBarLeftMargin + ($progress * $goalBarWidth) : $goalBarLeftMargin + $goalBarWidth;
+
+  foreach ($colorSets as $colorSet) {
+
+    $draw = new ImagickDraw();
+    $draw->setFontSize(15);
+    $draw->setStrokeAntialias(TRUE);
+    $draw->setTextAntialias(TRUE);
+    $draw->setTextAlignment(\Imagick::ALIGN_LEFT);
+    $draw->annotation($goalBarLeftMargin, $imageHeight - $goalBarBottomMargin + $bottomTextOffset, "0");
+    $draw->setTextAlignment(\Imagick::ALIGN_RIGHT);
+    $draw->annotation($imageWidth - $goalBarRightMargin, $imageHeight - $goalBarBottomMargin + $bottomTextOffset, "$goal");
+    $draw->setTextAlignment(\Imagick::ALIGN_CENTER);
+    $draw->annotation($progressChange, $goalBarTopMargin - $topTextOffset, "$count");
 
 
+    $draw->setStrokeColor($colorSet["done"]);
+    $draw->setFillColor($colorSet["done"]);
+    $draw->roundRectangle(
+      $goalBarLeftMargin, $goalBarTopMargin,
+      $progressChange, $imageHeight - $goalBarBottomMargin,
+      $goalBarRounding, $goalBarRounding);
 
-$canvas = new Imagick();
-$canvas->newImage($imageWidth, $imageHeight, $colorTransparent);
+    $draw->setStrokeColor($colorSet["upcoming"]);
+    $draw->setFillColor($colorSet["upcoming"]);
+    $draw->roundRectangle(
+      $progressChange, $goalBarTopMargin,
+      $imageWidth - $goalBarRightMargin, $imageHeight - $goalBarBottomMargin,
+      $goalBarRounding, $goalBarRounding);
 
-$canvas->drawImage($draw);
-$canvas->setImageFormat('png');
+    $topTriangle = [
+      [
+        'x' => $progressChange - $triangleSize,
+        'y' => $goalBarTopMargin - $triangleOffset,
+      ],
+      [
+        'x' => $progressChange + $triangleSize,
+        'y' => $goalBarTopMargin - $triangleOffset,
+      ],
+      [
+        'x' => $progressChange,
+        'y' => $goalBarTopMargin + $triangleSize - $triangleOffset,
+      ],
+    ];
+    $bottomTriangle = [
+      [
+        'x' => $progressChange - $triangleSize,
+        'y' => $imageHeight - $goalBarBottomMargin + $triangleOffset,
+      ],
+      [
+        'x' => $progressChange + $triangleSize,
+        'y' => $imageHeight - $goalBarBottomMargin + $triangleOffset,
+      ],
+      [
+        'x' => $progressChange,
+        'y' => $imageHeight - $goalBarBottomMargin - $triangleSize + $triangleOffset,
+      ],
+    ];
 
-/* Output the image */
-header("Content-Type: image/png");
-echo $canvas;
+
+    $draw->setStrokeAntialias(TRUE);
+    $draw->setStrokeColor($colorSet["divider"]);
+    $draw->setFillColor($colorSet["divider"]);
+    $draw->polygon($topTriangle);
+    $draw->polygon($bottomTriangle);
+    $draw->line($topTriangle[2]['x'], $topTriangle[2]['y'], $bottomTriangle[2]['x'], $bottomTriangle[2]['y']);
+
+
+    $canvas = new Imagick();
+    $canvas->newImage($imageWidth, $imageHeight, $colorTransparent);
+
+    $canvas->drawImage($draw);
+    $canvas->setImageFormat('png');
+
+    /* Output the image */
+    $canvas->writeImage("progressbar-" . $colorSet["name"] . "-" . $sizeSet["name"] . ".png");
+  }
+}
 ?>
 
